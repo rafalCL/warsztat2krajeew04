4efb061f8b98199200d96d75e0cb509dfc90ea7c
Rafal <rafal.opial@coderslab.pl>
2020-01-19 16:34:15 +0100

    Hash password

--- src/pl/coderslab/warsztat2krajeew04/model/User.java
+++ src/pl/coderslab/warsztat2krajeew04/model/User.java
@@ -1,5 +1,7 @@
 package pl.coderslab.warsztat2krajeew04.model;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class User {
     private int id;
     private String username;
@@ -9,7 +11,7 @@ public class User {
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
-        this.password = password;
+        setPassword(password);
     }
 
     public User() {
@@ -45,6 +47,10 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public boolean checkPassword(String plain){
+        return BCrypt.checkpw(plain, this.password);
     }
 }
